419d52f2a3af77539e459d880e18a2f4b9686b08
Rafael <rafaelddem@gmail.com>
2026-05-19 12:37:48 -0300

    Separado ambiente de desenvolvimento/producao

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-DOMINIO="rafaelsolucoesdigitais.com.br"
+if [ "$ENVIRONMENT" = "dev" ]; then
+    echo "Ambiente de Desenvolvimento detectado. Usando localhost."
+    DOMINIO="localhost"
+    DAYS_TO_EXPIRE=365
+else
+    echo "Ambiente de Produção/Padrão detectado."
+    DOMINIO="rafaelsolucoesdigitais.com.br"
+    DAYS_TO_EXPIRE=1
+fi
+
 CERT_DIR="/etc/letsencrypt/live/$DOMINIO"
 
 if [ ! -f "$CERT_DIR/fullchain.pem" ]; then
-    echo "Certificado real não encontrado. Criando certificado temporário..."
+    echo "Certificado não encontrado. Criando certificado para $DOMINIO..."
     mkdir -p "$CERT_DIR"
-    openssl req -x509 -nodes -days 1 -newkey rsa:2048 \
+    openssl req -x509 -nodes -days $DAYS_TO_EXPIRE -newkey rsa:2048 \
         -keyout "$CERT_DIR/privkey.pem" \
         -out "$CERT_DIR/fullchain.pem" \
-        -subj "/CN=localhost"
+        -subj "/CN=$DOMINIO"
 fi
 
 echo "Iniciando o Nginx normalmente..."
